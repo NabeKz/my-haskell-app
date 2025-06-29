b4dab005a4d4892e733fa32a498bdab23dbd5fa9
NabeKz <ayuzak4me38@gmail.com>
2025-06-30 01:46:12 +0900

    Refactor project structure: update dependencies, add request file, and remove unused stack.yaml

--- src/Api.hs
+++ src/Api.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Api where
 
@@ -9,18 +9,21 @@ import GHC.Generics
 import Servant
 
 data User = User
-  { userId :: Int
-  , userName :: String
-  , userEmail :: String
-  } deriving (Eq, Show, Generic)
+  { userId :: Int,
+    userName :: String,
+    userEmail :: String
+  }
+  deriving (Eq, Show, Generic)
 
 instance ToJSON User
+
 instance FromJSON User
 
-type API = "users" :> Get '[JSON] [User]
-      :<|> "users" :> ReqBody '[JSON] User :> Post '[JSON] User
-      :<|> "users" :> Capture "id" Int :> Get '[JSON] User
-      :<|> "health" :> Get '[JSON] String
+type API =
+  "users" :> Get '[JSON] [User]
+    :<|> "users" :> ReqBody '[JSON] User :> Post '[JSON] User
+    :<|> "users" :> Capture "id" Int :> Get '[JSON] User
+    :<|> "health" :> Get '[JSON] String
 
 api :: Proxy API
 api = Proxy
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,13 @@
 module Lib
-    ( startApp
-    , app
-    ) where
+  ( startApp,
+    app,
+  )
+where
 
+import Api
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
-import Api
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -18,16 +19,17 @@ server :: Server API
 server = getUsers :<|> postUser :<|> getUser :<|> getHealth
   where
     getUsers :: Handler [User]
-    getUsers = return
-      [ User 1 "Alice" "alice@example.com"
-      , User 2 "Bob" "bob@example.com"
-      ]
-    
+    getUsers =
+      return
+        [ User 1 "Alice" "alice@example.com",
+          User 2 "Bob" "bob@example.com"
+        ]
+
     postUser :: User -> Handler User
     postUser user = return user
-    
+
     getUser :: Int -> Handler User
     getUser uid = return $ User uid "User" "user@example.com"
-    
+
     getHealth :: Handler String
     getHealth = return "OK"
