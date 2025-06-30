4c35ee9567011d239a3ab1b384c9ee5cbc59c595
NabeKz <ayuzak4me38@gmail.com>
2025-06-30 22:20:53 +0900

    Refactor GetBooks module: consolidate repository interface and implementation, streamline service logic, and improve error handling

--- src/Slices/Books/GetBooks.hs
+++ src/Slices/Books/GetBooks.hs
@@ -3,34 +3,37 @@
 
 module Slices.Books.GetBooks where
 
+import qualified Data.ByteString.Lazy.Char8 as L8
 import Servant
 import Slices.Books.Types
-import qualified Data.ByteString.Lazy.Char8 as L8
 
 -- API Definition
 type GetBooksAPI = "books" :> Get '[JSON] [Book]
 
--- Data Access (Repository)
-getAllBooksFromRepo :: IO [Book]
-getAllBooksFromRepo = return
-  [ Book (BookId 1) "Functional Programming in Haskell"
-  ]
+-- Repository Interface
+class BookRepository m where
+  getAllBooks :: m [Book]
 
--- Business Logic (Service)
-getAllBooksService :: IO (BookResult [Book])
+-- In-Memory Repository Implementation
+data InMemoryBookRepo = InMemoryBookRepo
+
+instance BookRepository IO where
+  getAllBooks =
+    return
+      [ Book (BookId 1) "Functional Programming in Haskell"
+      ]
+
+-- Business Logic (Service) with DI
+getAllBooksService :: (Monad m, BookRepository m) => m (BookResult [Book])
 getAllBooksService = do
-  books <- getAllBooksFromRepo
-  return $ Right books
+  Right <$> getAllBooks
 
 -- HTTP Handler
 getBooksHandler :: Handler [Book]
 getBooksHandler = do
-  result <- liftIO getAllBooksService
+  result <- liftIO (getAllBooksService :: IO (BookResult [Book]))
   case result of
-    Left _ -> throwError err500 { errBody = L8.pack "Internal server error" }
+    Left _ -> throwError err500 {errBody = L8.pack "Internal server error"}
     Right books -> return books
   where
     liftIO = liftIO
-    throwError = throwError
-    err500 = err500
-    errBody = errBody
