750e38228b19569efbc459301d85f88ccbaa63be
NabeKz <ayuzak4me38@gmail.com>
2025-06-30 20:31:52 +0900

    Refactor server functions: move handler definitions to top-level scope

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,24 +12,24 @@ import Servant
 startApp :: IO ()
 startApp = run 8080 app
 
+getUsers :: Handler [User]
+getUsers =
+  return
+    [ User 1 "Alice" "alice@example.com",
+      User 2 "Bob" "bob@example.com"
+    ]
+
+postUser :: User -> Handler User
+postUser user = return user
+
+getUser :: Int -> Handler User
+getUser uid = return $ User uid "User" "user@example.com"
+
+getHealth :: Handler String
+getHealth = return "OK"
+
 app :: Application
 app = serve api server
 
 server :: Server API
 server = getUsers :<|> postUser :<|> getUser :<|> getHealth
-  where
-    getUsers :: Handler [User]
-    getUsers =
-      return
-        [ User 1 "Alice" "alice@example.com",
-          User 2 "Bob" "bob@example.com"
-        ]
-
-    postUser :: User -> Handler User
-    postUser user = return user
-
-    getUser :: Int -> Handler User
-    getUser uid = return $ User uid "User" "user@example.com"
-
-    getHealth :: Handler String
-    getHealth = return "OK"
